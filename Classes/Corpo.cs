10c6b5618a25bdb4b4e541b965028041bec39f9f
= <=>
2022-04-22 00:12:32 -0300

    Classe Corpo e Universo refeitas, cálculos ainda estão errados

--- Classes/Corpo.cs
+++ Classes/Corpo.cs
@@ -5,8 +5,6 @@ namespace TrabalhoN1.Classes
 {
     public class Corpo
     {
-        
-        double G = 6.674184 * Math.Pow(10,-11);
         public string Nome { get; set; }
         public double Massa { get; set; }
         public double Raio { get; set; }
@@ -14,75 +12,13 @@ namespace TrabalhoN1.Classes
         public double PosY { get; set; }
         public double VelX { get; set; }
         public double VelY { get; set; }
+        public double ForcaX { get; set; }
+        public double ForcaY { get; set; }
 
 
         public Corpo()
         {
             Nome = "";
-        }
-        public double Aceleracao(double forca)
-        {
-            double a = 0;
-            a = forca * Massa;
-            return a;
-        }
-        public double Distancia(Corpo corpoVizinho)
-        {
-            double d = 0;
-            double x2 = Math.Pow((corpoVizinho.PosX - this.PosX),2);
-            double y2 = Math.Pow((corpoVizinho.PosY - this.PosY),2);
-            d = Math.Sqrt(x2+y2);
-            return d;
-        }
-        public double Forca(Corpo corpoVizinho)
-        {
-            double f = 0;
-            f = G*(Massa*corpoVizinho.Massa)/Distancia(corpoVizinho);
-            return f;
-
-        }
-        public double Angulo(Corpo corpoVizinho)
-        {
-            double angulo = 0;
-            double x2 = corpoVizinho.PosX;
-            // theta = ACos((x1-x2)/Hipotenusa)
-            // hipotenusa = Distancia()
-            double anguloRadiano = Math.Acos((this.PosX-x2)/Distancia(corpoVizinho)); // Math.Acos() retorna angulo em radianos
-            angulo = anguloRadiano * 180 / Math.PI; // transformando de radianos para graus
-
-            return angulo;
-        }
-        public void Velocidade(Corpo[] CorposCelestes, double tempo)
-        {
-            // nao esta completo
-            double[] velXArray = new double[CorposCelestes.Length-1];
-            double[] velYArray = new double[CorposCelestes.Length-1];
-
-            foreach (Corpo corpoVizinho in CorposCelestes)
-            {
-                int count = 0;
-                if (corpoVizinho != this)
-                {
-                    // V = v0 * a * t
-                    // a = f * t
-                    
-                    double Fx = Forca(corpoVizinho) * Math.Cos(Angulo(corpoVizinho));
-                    double Fy = Forca(corpoVizinho) * Math.Sin(Angulo(corpoVizinho));
-
-                    velXArray[count] = VelX + Aceleracao(Fx) * tempo;
-                    velYArray[count] = VelY + Aceleracao(Fy) * tempo;
-
-                    count++;
-                }
-            }
-
-            this.VelX = this.VelX + velXArray.Sum();
-            this.VelY = this.VelY + velYArray.Sum();
-        }
-        public void CalcularPosicao(double Tempo)
-        {
-            PosX = PosX + VelX * Tempo;
-            PosY = PosY + VelY * Tempo;
         }
     }
 }
--- Classes/Universo.cs
+++ Classes/Universo.cs
@@ -9,10 +9,48 @@ namespace TrabalhoN1.Classes
         {
             for (int i = 0; i < CorposCelestes.Length; i++)
             {
-                CorposCelestes[i].Velocidade(CorposCelestes, Tempo);
-                CorposCelestes[i].CalcularPosicao(Tempo);
+                for (int j = 0; j < CorposCelestes.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        CalculaForcas(CorposCelestes[i],CorposCelestes[j]);
+                    }
+                }
+                CalcularPosicao(CorposCelestes[i]);
+                CalcularVelocidade(CorposCelestes[i]);
             }
         }
+        private double CalculaDistancia(Corpo corpo1, Corpo corpo2)
+        {
+            double d = 0;
+            double x2 = Math.Pow((corpo1.PosX - corpo2.PosX),2);
+            double y2 = Math.Pow((corpo1.PosY - corpo2.PosY),2);
+            d = Math.Sqrt(x2+y2);
+            return d; //572.7704601321545
+        }
+        public void CalculaForcas(Corpo corpo1, Corpo corpo2)
+        {
+            double G = 6.674184 * Math.Pow(10,-11);
+            double Forca = 0;
+            double distancia =  CalculaDistancia(corpo1, corpo2);
+            Forca = G*(corpo1.Massa*corpo2.Massa)/Math.Pow(distancia,2); //6.074056279975799E-06
+            
+            corpo1.ForcaX += Forca * (corpo1.PosX - corpo2.PosX) / distancia; //6.055281089506516E-06
+            corpo1.ForcaY += Forca * (corpo1.PosY - corpo2.PosY) / distancia; //-4.772112942693402E-07
+        }
+        public void CalcularPosicao(Corpo corpo)
+        {
+            double ax = (corpo.ForcaX * corpo.Massa);
+
+            corpo.PosX += (corpo.VelX * Tempo) + ((corpo.ForcaX * corpo.Massa)/2) * (Tempo*Tempo); //1264.39165689528285
+            corpo.PosY += (corpo.VelY * Tempo) + ((corpo.ForcaY * corpo.Massa)/2) * (Tempo*Tempo); //391.2458414005469
+        }
+        public void CalcularVelocidade(Corpo corpo)
+        {
+            corpo.VelX += corpo.ForcaX * corpo.Massa;
+            corpo.VelY += corpo.ForcaY * corpo.Massa;
+        }
+
         public string LogIteracao(Corpo[] CorposCelestes)
         {
             string log = string.Empty;
